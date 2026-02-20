ce6cacc3f75418b9234df6be796b2a5fbf0268fc
deribrediet-eng <deribrediet@gmail.com>
2026-02-19 18:23:21 -0800

    I made the same mistake intentionally.

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React,{ useState} from 'react';
+import {view, text , textInput, button ,stylesheet } from 'rect-native';
 
 export default function App() {
-  const [message, setMessage] = useState("ይጫኑ");
   return (
-    <View style={styles.container}>
-      <Text onPress={() => setMessage(" ትጭነዋል!")} style={{fontSize: 24}}>{message}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <view>
+      <text> My Tasks </text>
+    </view>
+  )
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#676c51',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
